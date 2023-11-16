fabfaa757c1a5e028c6a1d9aa9c2ee8b66e92037
Srijit <srijitmishra@protonmail.com>
2023-11-16 16:23:47 +0530

    initial setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
+import { store } from "./store";
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
